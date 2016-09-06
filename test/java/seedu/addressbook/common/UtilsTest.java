95794206e7392caf2d05634bcbc35f987d8561c7
Karen Ang <thekarenang@gmail.com>
2016-09-07 00:20:46 +0800

    Add tests for elementsAreUnique

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -21,9 +21,40 @@ public class UtilsTest {
 		assertTrue(Utils.isAnyNull(itemIsNull));
 		assertFalse(Utils.isAnyNull(itemIsNotNull));
 	}
-	
+
 	@Test
 	public void isAnyNull_checksMultipleItemsCorrectly() {
 		assertTrue(Utils.isAnyNull(itemIsNull, itemIsNotNull));
+	}
+
+	@Test
+	public void elementsAreUnique_checksCorrectlyWithEmptyCollection() {
+		assertTrue(Utils.elementsAreUnique(new ArrayList<Integer>()));
+	}
+
+	@Test
+	public void elementsAreUnique_checksCorrectlyUniqueCollection() {
+		ArrayList<Integer> integers = new ArrayList<Integer>();
+		integers.add(1);
+		integers.add(2);
+		integers.add(3);
+		assertTrue(Utils.elementsAreUnique(integers));
+		ArrayList<String> strings = new ArrayList<String>();
+		strings.add("a");
+		strings.add("b");
+		strings.add("c");
+		assertTrue(Utils.elementsAreUnique(strings));
+	}
+
+	@Test
+	public void elementsAreUnique_checksCorrectlyDuplicateCollection() {
+		ArrayList<Integer> integers = new ArrayList<Integer>();
+		integers.add(1);
+		integers.add(1);
+		assertFalse(Utils.elementsAreUnique(integers));
+		ArrayList<String> strings = new ArrayList<String>();
+		strings.add("a");
+		strings.add("a");
+		assertFalse(Utils.elementsAreUnique(strings));
 	}
 }
