2424ba1d6c9ee7f0e8eb2825f49f7101b89421a5
Karen Ang <thekarenang@gmail.com>
2016-09-06 10:28:51 +0800

    Extract commonalities among `Address`, `Email` and `Phone` into `Contact`.

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+	private boolean isPrivate;
+
+	public boolean isPrivate() {
+		return isPrivate;
+	}
+
+	public void setPrivate(boolean isPrivate) {
+		this.isPrivate = isPrivate;
+	}
+
+}
